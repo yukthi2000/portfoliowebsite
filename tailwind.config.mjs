66ca61abb484f4c72e2296924b9f395a7180d3fc
Yukthi <yukthihettiarachchissck@gmail.com>
2025-03-02 10:10:43 +0530

    added a tailwind css

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+export default {
+  theme: {
+    extend: {
+      colors: {
+        lightHover: '#f3f4f6',
+        darkHover: '#2a004a',
+        darkTheme: '#1a202c',
+      },
+      fontFamily: {
+        // Add your custom fonts here
+      },
+    },
+  },
+  plugins: [],
+}
